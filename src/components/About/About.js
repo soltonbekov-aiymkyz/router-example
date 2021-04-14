379fe93577c5192ba903cafa07e5ae0c8a846b0f
Your Name <you@example.com>
2021-04-14 02:09:41 -0700

    lsdfkl

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,6 +22,37 @@ const About =()=> {
         conubia nostra, per inceptos himenaeos.
       </p>
         
+      <p className="classes.p" >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
+        dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
+        Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
+        commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
+        Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
+        at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
+        Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
+        Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
+        efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
+        venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
+        sollicitudin. Class aptent taciti sociosqu ad litora torquent per
+        conubia nostra, per inceptos himenaeos.
+      </p>
+      
+      <p className="classes.p" >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
+        dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
+        Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
+        commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
+        Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
+        at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
+        Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
+        Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
+        efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
+        venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
+        sollicitudin. Class aptent taciti sociosqu ad litora torquent per
+        conubia nostra, per inceptos himenaeos.
+      </p>
         
         </div>
     )
