1cecbca530fabae51de1a3f07da35ae7d0beca4f
Your Name <you@example.com>
2021-04-14 07:07:36 -0700

    added some package

---
+++ src/components/Products/Products.js
@@ -0,0 +1,91 @@
+import classes from "./Products.module.css";
+// import img from "../../images/envelope.svg";
+import img from "../../Images/abondoned.jpg"
+const Products = () => {
+  return (
+    <div className={classes.Economy}>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
