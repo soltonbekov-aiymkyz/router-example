cb67d27d7c10a6603b4aa36306dee283c3081aaa
Your Name <you@example.com>
2021-04-14 04:21:48 -0700

    added some photos for cards

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -7,7 +7,7 @@ const Contacts =()=> {
         <h2>This is contacts page</h2>
         <p className={classes.con}>Us contacts</p>
         <span>
-            <p>+ 996 702 3455 </p>
+            <p>+ 996 702 3455 (WhatsApp) </p>
             <p>+ 234 56545634 </p>
         </span>
     </div>
