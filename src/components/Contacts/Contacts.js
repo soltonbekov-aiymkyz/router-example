7e042dfe26a159ecc7c195c975ea48dc2c66fb90
Your Name <you@example.com>
2021-04-14 07:05:52 -0700

    lorem ipsum dolor sit amet

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,17 +1,15 @@
 
-
 import classes from "./Contacts.module.css";
 const Contacts =()=> {
     return(
         <div className={classes.Contacts}>
-        <h2>This is contacts page</h2>
+        <h2> Contacts </h2>
         <p className={classes.con}>our contacts</p>
         <span>
             <p>+ 996 702 23 34 55 (WhatsApp) </p>
             <p>+ 234 56545634 </p>
         </span>
     </div>
-
     )
 }
 export default Contacts;
