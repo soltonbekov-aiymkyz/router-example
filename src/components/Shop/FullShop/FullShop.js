0d66effbd99877fde0612c01c69ac860df17e1fa
Your Name <you@example.com>
2021-04-14 04:56:34 -0700

    rename this page name

---
+++ src/components/Shop/FullShop/FullShop.js
@@ -0,0 +1,14 @@
+import { Route } from "react-router";
+import classes from "./FullShop.module.css";
+import { Link } from "react-router-dom";
+
+const FullShop = ({ match }) => {
+    return ( 
+        <div className={classes.FullShop}>
+            <Link to="/shop">Back to posts</Link>
+            <h1>News #{match.params.id}</h1>
+        </div>
+    );
+}
+ 
+export default FullShop;
