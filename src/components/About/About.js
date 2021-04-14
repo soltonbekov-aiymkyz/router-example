aa062e53c0d6763abba4228294e4b0858fbf6932
Your Name <you@example.com>
2021-04-14 04:21:08 -0700

    added some photos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,61 +1,67 @@
 
 import classes from "./About.module.css";
-
-const About =()=> {
-    return(
-        <div className={classes.About}>
-            <h1> about </h1>
-            <span>about us </span>
-            <p className="classes.p" >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
-        dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
-        Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
-        commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
-        Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
-        at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
-        Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
-        Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
-        efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
-        venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
-        sollicitudin. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos.
-      </p>
+const About = () => {
+    return (
+      <div className={classes.About}>
+        <h2>About us</h2>
+        <div className={classes.divp}>
+          <div className={classes.section}>
+            <h3>Title</h3>
+            <p className={classes.par}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
+            dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
+            Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
+            commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
+            Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
+            at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
+            Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
+            Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
+            efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
+            venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
+            nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
+            sollicitudin. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos.
+          </p></div>
+          <div className={classes.section}>
+            <h3>Title</h3>
+            <p className={classes.par}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
+            dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
+            Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
+            commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
+            Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
+            at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
+            Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
+            Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
+            efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
+            venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
+            nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
+            sollicitudin. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos.
+          </p></div>
         
-      <p className="classes.p" >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
-        dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
-        Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
-        commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
-        Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
-        at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
-        Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
-        Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
-        efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
-        venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
-        sollicitudin. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos.
-      </p>
-      
-      <p className="classes.p" >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
-        dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
-        Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
-        commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
-        Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
-        at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
-        Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
-        Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
-        efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
-        venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
-        sollicitudin. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos.
-      </p>
         
+       
+          <div className={classes.section}>
+            <h3>some something</h3>
+            <p className={classes.par}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec
+            dignissim risus. Sed elit ex, dignissim ac est nec, porta rutrum lorem.
+            Aenean maximus vestibulum ullamcorper. Aliquam lobortis, odio a suscipit
+            commodo, dolor tortor mattis urna, eu pretium enim ipsum eget turpis.
+            Aliquam aliquam, orci sed bibendum tristique, velit nisl facilisis orci,
+            at luctus est eros in sapien. Aenean et nulla at mi accumsan vehicula.
+            Nunc tristique dolor quis odio luctus, consequat lacinia ante tempus.
+            Praesent imperdiet laoreet libero eget tincidunt. Morbi elementum
+            efficitur nisi nec malesuada. Integer commodo turpis nec faucibus
+            venenatis. Class aptent taciti sociosqu ad litora torquent per conubia
+            nostra, per inceptos himenaeos. Nulla aliquet enim ut pretium
+            sollicitudin. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos.
+          </p></div>
         </div>
-    )
-}
-export default About;
-//state props router ajax
+      </div>
+    );
+  };
+  
+  export default About;
