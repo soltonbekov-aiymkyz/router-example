9c1c811fe503e12d4e27f7aa26935681689ce2b2
Your Name <you@example.com>
2021-04-14 04:56:04 -0700

    delete some functions

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,16 @@
-const Shop =()=> {
-    return(
-        <div>this is Shop page</div>
-        
-    )
+import { Route } from "react-router";
+import FullShop from "./FullShop/FullShop";
+import classes from "./Shop.module.css";
+import ShopList from "./ShopList/ShopList";
+
+const Shop = ({ match }) => {
+    return ( 
+        <div className={classes.Shop}>
+            { match.isExact
+            ? <ShopList />
+            : <Route path="/shop/:id" component={FullShop} /> }
+        </div>
+    );
 }
+ 
 export default Shop;
